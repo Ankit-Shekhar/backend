7f623793224080856309b56731dc119c67822450
Ankit Shekhar <ankitshekhar.23@nshm.edu.in>
2025-07-03 22:31:18 +0530

    connect to MongoDB Atlas

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from '../constants.js';
+
+const connectDb = async () => {
+    try {
+        const connectionInsance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+
+        console.log(`Database connected successfully! BD HOST: ${connectionInsance.connection.host}`);
+    } catch (error) {
+        console.log(`Database connection Failed: ${error}`);
+        process.exit(1); // Exit the process with failure
+    }
+}
+
+export default connectDb
---
+++ src/main.js
@@ -0,0 +1,37 @@
+// require('dotenv').config({path: './.env'}); // Load environment variables from .env file
+import dotenv from "dotenv"
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDb from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+});
+
+connectDb()
+
+
+
+
+
+
+
+
+
+
+
+
+// //always use "async await" and "try catch" when dealing with database
+
+// // "ifis" statement is a way to write functions: below is an example.
+
+// (async ()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//     } catch (error) {
+//         console.log(`ERROR: ${error}`);
+//     }
+// })()
+
+//The above can be done but a more better practise is in the "index.js"
