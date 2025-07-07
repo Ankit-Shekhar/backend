b143f3b85e9956130a064916a0c448bd67ee8502
Ankit Shekhar <ankitshekhar.23@nshm.edu.in>
2025-07-07 17:14:18 +0530

    Util code written

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,9 @@ import { DB_NAME } from "../constants.js";
 
 const connectDb = async () => {
     try {
-        const connectionInsance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
 
-        console.log(`Database connected successfully! BD HOST: ${connectionInsance.connection.host}`);
+        console.log(`Database connected successfully! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log(`Database connection Failed: ${error}`);
         process.exit(1); // Exit the process with failure
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+
+//try catch method
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(err.code || 500).json({
+//        success: false,
+//        message: err.message || 'An error occurred'
+//     });
+//   }
+// }
+
+// export default asyncHandler;
+
+
+//Promise method
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+    .catch((err) => next(err));
+  }
+}
+export { asyncHandler }
